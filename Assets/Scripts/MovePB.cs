19bab66047e46dca4ab97b44e35720b9462db207
KeweiQ <kewei.qiu@outlook.com>
2022-03-01 14:23:42 -0500

    basic version of new movement according to camera heading

--- Assets/Scripts/MovePB.cs
+++ Assets/Scripts/MovePB.cs
@@ -4,23 +4,34 @@ using UnityEngine;
 
 public class MovePB : MonoBehaviour
 {
+    // user inputs
     private float _playerInput;
     private float _rotationInput;
-    private Vector3 _userRot;
-    private bool _userJumped;
-    private bool _jumpInProgress = false;
 
+    // camera and character heading related
+    public GameObject MainCamera;
+    public Transform CameraTransform;
+    private Vector3 _userRot;
+    private Vector3 _cameraRot;
+    private Vector3 heading;
+
+    // tune sensitivity of controls
     // original mass, drag, angularDrag: 1, 2, 0.05;
     private float MoveScale = 0.5f; // original 0.5
     private const float RotateScale = 3.0f; // original 1.0
-    private float distanceToGround;
     private const float JumpMultiplier = 4.0f; // original 1.6
     private const float MaxSpeed = 5.0f;
 
+    // jump limiter
+    private bool _userJumped;
+    private bool _jumpInProgress = false;
+    private float distanceToGround;
+
+    // model components
     private Rigidbody _rigidbody;
     private Transform _transform;
 
-    // Animation related
+    // animation related
     Animator animator;
     bool moving_forward;
     bool is_grounded;
@@ -34,15 +45,19 @@ public class MovePB : MonoBehaviour
         animator = GetComponent<Animator>();
         distanceToGround = GetComponent<Collider>().bounds.extents.y;
         // StartCoroutine(printStates());
+        CameraTransform = MainCamera.GetComponent<Transform>();
     }
 
     void Update()
     {
+        // get keyboard inputs
         _playerInput = Input.GetAxis("Vertical");
         _rotationInput = Input.GetAxis("Horizontal");
         _userJumped = Input.GetButton("Jump");
 
+        // play animations according to keyboard inputs
         moving_forward = Input.GetKey("w") || Input.GetKey("s") ||
+                          Input.GetKey("a") || Input.GetKey("d") ||
                           Input.GetKey("up") || Input.GetKey("down");
         is_grounded = IsGrounded();
         jumping  = Input.GetKey("space");
@@ -51,27 +66,57 @@ public class MovePB : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // decide which direction should the character go (according to camera heading)
         _userRot = _transform.rotation.eulerAngles;
-        _userRot += new Vector3(0, _rotationInput * RotateScale, 0);
-
+        _cameraRot = CameraTransform.rotation.eulerAngles;
+        if (Input.GetKey("a")) {
+            _userRot[1] = _cameraRot[1];
+            _userRot += new Vector3(0, -90, 0);
+            heading = _transform.forward;
+        } else if (Input.GetKey("d")) {
+            _userRot[1] = _cameraRot[1];
+            _userRot += new Vector3(0, 90, 0);
+            heading = _transform.forward;
+        } else if (Input.GetKey("w")) {
+            _userRot[1] = _cameraRot[1];
+            heading = _transform.forward;
+        } else if (Input.GetKey("s")) {
+            _userRot[1] = _cameraRot[1];
+            _userRot += new Vector3(0, 180, 0);
+            heading = _transform.forward;
+        }
+        
+        // rotate character to the right direction
         _transform.rotation = Quaternion.Euler(_userRot);
 
+        //heading = new Vector3(Mathf.Abs(heading.x), Mathf.Abs(heading.y), Mathf.Abs(heading.z));
+        float inputScale = 0;
+        if (_playerInput != 0 && _rotationInput > 0) {
+            inputScale = (Mathf.Abs(_playerInput) + Mathf.Abs(_rotationInput)) / 2.0f;
+        } else if (_playerInput != 0 && _rotationInput == 0) {
+            inputScale = Mathf.Abs(_playerInput);
+        } else if (_playerInput == 0 && _rotationInput != 0) {
+            inputScale = Mathf.Abs(_rotationInput);
+        } else {
+            inputScale = 0;
+        }
+
+        // let the character go forward
         if (sprinting)
         {
-          _rigidbody.velocity += transform.forward * _playerInput *MoveScale * 2;
+            _rigidbody.velocity += heading * inputScale * MoveScale * 1.5f;
         }
         else
         {
-          _rigidbody.velocity += transform.forward * _playerInput * MoveScale;
+            _rigidbody.velocity += heading * inputScale * MoveScale;
         }
 
+        // perform animations
         animator.SetBool("isWalking", moving_forward);
         animator.SetBool("isJumping", jumping);
         animator.SetBool("IsGrounded", is_grounded);
         animator.SetBool("isRunning", sprinting);
         animator.SetBool("isIdle", !moving_forward && is_grounded);
-
-
 
         // Only able to jump if you are on the ground
         if (is_grounded && _userJumped)
