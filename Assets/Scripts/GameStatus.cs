615db6ed18d0e59bfaf4126a706a71ee42b2be81
Arya Rafii <aryrafii@amd.com>
2022-03-02 17:35:56 -0500

    Updated UI time.

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -16,6 +16,7 @@ public class GameStatus : MonoBehaviour
     private Text gameOperText;
 
     public float timeLeft = 90;
+    private float totalTime;
     public Slider slider;
     public bool winStat = false;
     private Rigidbody _rigidbody;
@@ -29,6 +30,7 @@ public class GameStatus : MonoBehaviour
         gameStatText.text = "";
         gameOperText.text = "";
         slider.value = 1f;
+        totalTime = timeLeft;
     }
 
     // Update is called once per frame
@@ -67,13 +69,7 @@ public class GameStatus : MonoBehaviour
     }
 
     void DisplayTime(float time) {
-        if (time < 0) {
-            time = 0;
-        } else {
-            float minutes = Mathf.FloorToInt(time / 60);
-            float seconds = Mathf.FloorToInt(time % 60);
-            timeRemainingText.text = "Time Remaining: " + string.Format("{0:00}:{1:00}", minutes, seconds);
-        }
+        slider.value = timeLeft / totalTime;
     }
 
     public void PauseGame(string type) {
