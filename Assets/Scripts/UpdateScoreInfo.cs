9e74220d080a0386c5cb6900634cb2c75470ebfb
Arya Rafii <aryrafii@amd.com>
2022-03-02 14:23:44 -0500

    Adding gems increasing in UI as collected

--- Assets/Scripts/UpdateScoreInfo.cs
+++ Assets/Scripts/UpdateScoreInfo.cs
@@ -7,12 +7,22 @@ public class UpdateScoreInfo : MonoBehaviour
 {
     public GameObject playerScoreObj;
 
+    public GameObject[] gemImages;
+    public Texture collectedGem;
+
     private Text _playerScoreText;
     // Start is called before the first frame update
     void Start()
     {
         _playerScoreText = playerScoreObj.GetComponent<Text>();
         _playerScoreText.text = "Score: 0";
+    }
+
+    public void UpdateScoreImage(int score)
+    {
+        gemImages[score].GetComponent<RectTransform>().scale.x = 0.4;
+        gemImages[score].GetComponent<RectTransform>().scale.y = 0.7;
+        gemImages[score].GetComponent<RawImage>().texture = collectedGem;
     }
 
     public void UpdateScoreText(int score)
