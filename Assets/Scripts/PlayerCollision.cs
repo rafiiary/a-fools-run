3e2990324fb6403d3a2cfc7ff65656d4cb7746e8
kawaitenshi <97797729+kawaitenshi@users.noreply.github.com>
2022-03-03 00:51:17 -0700

    removed dragon transformation for now

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -6,7 +6,7 @@ public class PlayerCollision : MonoBehaviour
 {
   public GameObject character1;
   public GameObject character2;
-  public GameObject character3;
+//  public GameObject character3;
   public static bool hitFinishLine;
 
   public GameObject camera;
@@ -21,8 +21,8 @@ public class PlayerCollision : MonoBehaviour
       transform.parent.gameObject.GetComponent<Spawn>().setActivePlayer(character1);
     else if (character2.activeSelf)
       transform.parent.gameObject.GetComponent<Spawn>().setActivePlayer(character2);
-    else if (character3.activeSelf)
-      transform.parent.gameObject.GetComponent<Spawn>().setActivePlayer(character3);
+//    else if (character3.activeSelf)
+//      transform.parent.gameObject.GetComponent<Spawn>().setActivePlayer(character3);
     hitFinishLine = false;
   }
 
@@ -43,22 +43,22 @@ public class PlayerCollision : MonoBehaviour
 
       } else if (character2.activeSelf) {
         print("Changing to character 3");
-        character3.transform.position = character2.transform.position;
-        character3.transform.rotation = character2.transform.rotation;
+        character1.transform.position = character2.transform.position;
+        character1.transform.rotation = character2.transform.rotation;
         character2.SetActive(false);
-        character3.SetActive(true);
-        transform.parent.gameObject.GetComponent<Spawn>().setActivePlayer(character3);
-        camera.GetComponent<CameraController>().PlayerTransform = character3.transform.Find("Focus");
-
-      } else if (character3.activeSelf) {
-        print("Changing to character 1");
-        character1.transform.position = character3.transform.position;
-        character1.transform.rotation = character3.transform.rotation;
-        character3.SetActive(false);
         character1.SetActive(true);
         transform.parent.gameObject.GetComponent<Spawn>().setActivePlayer(character1);
         camera.GetComponent<CameraController>().PlayerTransform = character1.transform.Find("Focus");
-      }
+        }
+//      else if (character3.activeSelf) {
+//        print("Changing to character 1");
+//        character1.transform.position = character3.transform.position;
+//        character1.transform.rotation = character3.transform.rotation;
+//        character3.SetActive(false);
+//        character1.SetActive(true);
+//        transform.parent.gameObject.GetComponent<Spawn>().setActivePlayer(character1);
+//        camera.GetComponent<CameraController>().PlayerTransform = character1.transform.Find("Focus");
+//      }
     }
 
     if (collision.collider.CompareTag("FinishLine")) {
